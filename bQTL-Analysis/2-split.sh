79dd037f1e25ab9b06acfdecd902b6c97eb0195f
jengelhorn <60544247+jengelhorn@users.noreply.github.com>
2025-04-10 16:49:32 +0200

    Update 2-split.sh

--- bQTL-Analysis/2-split.sh
+++ bQTL-Analysis/2-split.sh
@@ -8,20 +8,20 @@ do
 	else loc="NPNRtoNA"
 	fi
 
-for v2 in "WW" DS
+for v2 in WW DS
 do
 
-head -n1 /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/$v2-CG_ratio.csv > /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/CG_head.csv
-head -n1 /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/$v2-CHG_ratio.csv > /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/CHG_head.csv
-head -n1 /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/$v2-CHH_ratio.csv > /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/CHH_head.csv
-head -n1 /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/$v2-MOA_peak_ratio.csv > /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/MOA_peak_head.csv
-head -n1 /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/$v2-ReadDepth_ratio.csv > /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/ReadDepth_head.csv
+head -n1 PATH_TO_DATA/BindingFrequency/$loc/$v2-CG_ratio.csv > PATH_TO_DATA/BindingFrequency/$loc/CG_head.csv
+head -n1 PATH_TO_DATA/BindingFrequency/$loc/$v2-CHG_ratio.csv > PATH_TO_DATA/BindingFrequency/$loc/CHG_head.csv
+head -n1 PATH_TO_DATA/BindingFrequency/$loc/$v2-CHH_ratio.csv > PATH_TO_DATA/BindingFrequency/$loc/CHH_head.csv
+head -n1 PATH_TO_DATA/BindingFrequency/$loc/$v2-MOA_peak_ratio.csv > PATH_TO_DATA/BindingFrequency/$loc/MOA_peak_head.csv
+head -n1 PATH_TO_DATA/BindingFrequency/$loc/$v2-ReadDepth_ratio.csv > PATH_TO_DATA/BindingFrequency/$loc/ReadDepth_head.csv
 
 
 v3="MOA_peak CG CHG CHH ReadDepth"
 
-	if [ ! -d "/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/splitted" ]
-	then mkdir -p "/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/splitted"
+	if [ ! -d "PATH_TO_DATA/BindingFrequency/$loc/splitted" ]
+	then mkdir -p "PATH_TO_DATA/BindingFrequency/$loc/splitted"
 	fi
 
 for i in {1..10}
@@ -40,25 +40,25 @@ do
 
 	if [ $v1 = "keep" ]
 	then
-	grep chr${i}_ /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/${k}-${l}_ratio.csv > /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/${k}-${l}_ratio_${i}.csv
-	cat /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/${l}_head.csv /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/${k}-${l}_ratio_${i}.csv > /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/splitted/${k}-${l}_ratio_${i}_file.csv
-	rm /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/${k}-${l}_ratio_${i}.csv
+	grep chr${i}_ PATH_TO_DATA/BindingFrequency/$loc/${k}-${l}_ratio.csv > PATH_TO_DATA/BindingFrequency/$loc/${k}-${l}_ratio_${i}.csv
+	cat PATH_TO_DATA/BindingFrequency/$loc/${l}_head.csv PATH_TO_DATA/BindingFrequency/$loc/${k}-${l}_ratio_${i}.csv > PATH_TO_DATA/BindingFrequency/$loc/splitted/${k}-${l}_ratio_${i}_file.csv
+	rm PATH_TO_DATA/BindingFrequency/$loc/${k}-${l}_ratio_${i}.csv
 	else
 # NPNPRtoNA
-	grep chr${i}_ /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/NPNRtoNA/${k}-${l}_ratio.csv > /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/NPNRtoNA/${k}-${l}_ratio_${i}.csv
-	cat /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/${l}_head.csv /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/NPNRtoNA/${k}-${l}_ratio_${i}.csv > /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/NPNRtoNA/splitted/${k}-${l}_ratio_${i}_file.csv
-	rm /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/NPNRtoNA/${k}-${l}_ratio_${i}.csv
+	grep chr${i}_ PATH_TO_DATA/BindingFrequency/NPNRtoNA/${k}-${l}_ratio.csv > PATH_TO_DATA/BindingFrequency/NPNRtoNA/${k}-${l}_ratio_${i}.csv
+	cat PATH_TO_DATA/BindingFrequency/$loc/${l}_head.csv PATH_TO_DATA/BindingFrequency/NPNRtoNA/${k}-${l}_ratio_${i}.csv > PATH_TO_DATA/BindingFrequency/NPNRtoNA/splitted/${k}-${l}_ratio_${i}_file.csv
+	rm PATH_TO_DATA/BindingFrequency/NPNRtoNA/${k}-${l}_ratio_${i}.csv
 	fi
 
 done 
 done
 done
 
-rm /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/CG_head.csv
-rm /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/CHG_head.csv
-rm /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/CHH_head.csv
-rm /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/MOA_peak_head.csv
-rm /netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/$loc/ReadDepth_head.csv
+rm PATH_TO_DATA/BindingFrequency/$loc/CG_head.csv
+rm PATH_TO_DATA/BindingFrequency/$loc/CHG_head.csv
+rm PATH_TO_DATA/BindingFrequency/$loc/CHH_head.csv
+rm PATH_TO_DATA/BindingFrequency/$loc/MOA_peak_head.csv
+rm PATH_TO_DATA/BindingFrequency/$loc/ReadDepth_head.csv
 
 done
 done
