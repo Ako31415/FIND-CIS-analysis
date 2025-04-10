6c0687a6f1fd4d991c2a39d9f03a76d7e4d5067d
jengelhorn <60544247+jengelhorn@users.noreply.github.com>
2025-04-10 16:24:16 +0200

    Update 1-zPush_A619_into_ReadDepth.jl

--- bQTL-Analysis/1-zPush_A619_into_ReadDepth.jl
+++ bQTL-Analysis/1-zPush_A619_into_ReadDepth.jl
@@ -1,9 +1,5 @@
 using CSV, DataFrames
 
-#miss = "NO"
-#a619 = "/Data/michael/MOA/Met_final/Normal_res/Raw_data/B73.A619.WW.q0_EG80.lowres.norm_read_count.p01.2AFP.csv"
-#peaks = "/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/MOA_QTL/BindingFrequency/NPNRtoNA/p01_2AFP-WW-a619_peak_ratio.csv"
-
 function addA619(a619, peaks)
 
                                     # bind the a619 ratio files with the A619 infomration 
@@ -25,13 +21,13 @@ function addA619(a619, peaks)
  end
  
 #WW
-addA619("/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/B73.A619.WW.2FPs.csv", "/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/NPNRtoValue/WW-ReadDepth_ratio.csv")
+addA619("PATH_TO_A619_DATA_WW/A619_WW.csv", "PATH_TO_DATA/BindingFrequency/NPNRtoValue/WW-ReadDepth_ratio.csv")
 
-# addA619("/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/B73.A619.WW.2FPs.csv", "/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/NPNRtoNA/WW-ReadDepth_ratio.csv")
+# addA619("PATH_TO_A619_DATA_WW/A619_WW.csv", "PATH_TO_DATA/BindingFrequency/NPNRtoNA/WW-ReadDepth_ratio.csv")
 
 
 #DS
-addA619("/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/B73.A619.DS.2FPs.csv", "/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/NPNRtoValue/DS-ReadDepth_ratio.csv")
+addA619("PATH_TO_A619_DATA_DS/A619_DS.csv", "PATH_TO_DATA/BindingFrequency/NPNRtoValue/DS-ReadDepth_ratio.csv")
 
-# addA619("/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/B73.A619.DS.2FPs.csv", "/netscratch/dep_psl/grp_frommer/Thomas/Results/HybMoa_0819_WWvsDS/bQTLs_25/NG_revision/BindingFrequency/NPNRtoNA/DS-ReadDepth_ratio.csv")
+# addA619("PATH_TO_A619_DATA_DS/A619_DS.csv", "PATH_TO_DATA/BindingFrequency/NPNRtoNA/DS-ReadDepth_ratio.csv")
 
