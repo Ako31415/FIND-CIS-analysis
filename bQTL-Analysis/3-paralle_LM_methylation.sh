db576332122176f9c9f75eb5bc6ecd8254906ba6
jengelhorn <60544247+jengelhorn@users.noreply.github.com>
2025-04-10 18:10:22 +0200

    Create 3-paralle_LM_methylation.sh

---
+++ bQTL-Analysis/3-paralle_LM_methylation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for k in WW DS
+do
+
+#source make sure R 4.4.1 is installed and the version that is used
+
+
+if [ ! -d PATH_TO_DATA/Results/NPNRtoValue/LM/$k ]
+then mkdir -p PATH_TO_DATA/Results/NPNRtoValue/LM/$k
+fi
+
+
+for i in {1..10}
+do
+	
+aom="PATH_TO_DATA/BindingFrequency/NPNRtoValue/splitted/${k}-MOA_peak_ratio_${i}_file.csv" 
+rd="PATH_TO_DATA/BindingFrequency/NPNRtoValue/splitted/${k}-ReadDepth_ratio_${i}_file.csv" 
+oneg="PATH_TO_DATA/GenotypeData_${k}/genotypes_divided_2FPs_${i}.csv"
+
+    
+				PATH_TO_JULIA/julia -t 1 \
+				3-LM_methylation.jl \
+				$aom $i $k $rd $oneg & 
+done
+done
